06ae7ffa6281c24954934d8ee6150554b0f74412
vansuphan <37709216+vansuphan@users.noreply.github.com>
2019-04-10 00:36:04 +0700

    add style.js

--- client.js
+++ client.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+    var htmlListUser = document.getElementById("list-user");
     var socket = io.connect('http://localhost:3000');
-    socket.on('user_online', function(arrClient){
-        console.log("Server emit to client", arrClient);
+    socket.on('even_user_online', function(arrClient){
+        var stringUser = renderListUser(arrClient);
+        htmlListUser.innerHTML = stringUser.join('');
+        console.log(htmlListUser);
+        //$(".list-user").html(renderListUser(arrClient));
     });
-})
+
+});
+function renderListUser(arr){
+    return arr.map(function(item){
+        return "<li>"+ item +"</li>";
+    });
+}
