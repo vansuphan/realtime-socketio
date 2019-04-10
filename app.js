2b9b11756ca4a79d2fb6c699fc34210a78f0fb54
vansuphan <37709216+vansuphan@users.noreply.github.com>
2019-04-10 16:38:56 +0700

    building feature login and user online

--- app.js
+++ app.js
@@ -21,18 +21,23 @@ var arrClient =[];
 
 io.on('connection', function (socket) {
     console.log('Co 1 Client connect', socket.id);
-    arrClient.push(socket.id);
-    let serverArrayUser = io.emit('user_online', arrClient);
+    let serverArrayUser = function(){
+        io.emit('even_user_online', arrClient);
+    }
     serverArrayUser();
-    console.log(arrClient);
 
     socket.on('disconnect', function(){
         let idRemove = arrClient.findIndex(function(item){
             return item == socket.id;
         });
         arrClient.splice(idRemove,1);
-        serverArrayUser();
         console.log("User", socket.id , " Da thoat ");
+        serverArrayUser();
     });
-    
+    socket.on("Client-sent-username", function(userName){
+        socket.name = userName;
+        arrClient.push(socket.name);
+        serverArrayUser();
+        console.log(arrClient);
+    });
 });
--- client.js
+++ client.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
+
     var htmlListUser = document.getElementById("list-user");
     var socket = io.connect('http://localhost:3000');
+
+    $(".main").hide();
+
+    $("#input-name-user").keyup(function (event) { 
+        if(event.keyCode === 13){
+            let name = $(this).val();
+            socket.emit("Client-sent-username", name);
+            $(".main").show();
+            $(".login").hide();
+        }
+        
+    });
+
     socket.on('even_user_online', function(arrClient){
         var stringUser = renderListUser(arrClient);
         htmlListUser.innerHTML = stringUser.join('');
-        console.log(htmlListUser);
-        //$(".list-user").html(renderListUser(arrClient));
+        $(".count-user").html("Co " + arrClient.length + " user online");
     });
-
+   
 });
 function renderListUser(arr){
     return arr.map(function(item){
