d101eeee6f370fdc603e5accd108a44ce4be3d4b
vansuphan <37709216+vansuphan@users.noreply.github.com>
2019-04-14 01:57:05 +0700

    make it better

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     var htmlListUser = document.getElementById("list-user");
-    var socket = io.connect('https://git.heroku.com/chat-realtime-v1.git');
+    var socket = io.connect('https://chat-realtime-v1.herokuapp.com/');
     var __account = localStorage.getItem("__account");
 
     $(".main").hide();
